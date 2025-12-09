b608b0acffa627613e1aa5786e540f980e922181
electropaskyda228 <118922240+electropaskyda228@users.noreply.github.com>
2025-12-09 16:42:55 +0300

    Add files via upload

---
+++ lab1/task4/lib/Common.ml
@@ -0,0 +1,24 @@
+module Common = struct
+  let max x y = if x > y then x else y
+
+  let rec pow10 e =
+    if e = 0 then 1
+    else 10 * pow10 (e - 1)
+  
+  let rec num_digits n =
+    if n = 0 then 1
+    else if n < 10 then 1
+    else 1 + num_digits (n / 10)
+  
+  let is_palindrome n =
+    let len = num_digits n in
+    let rec check i =
+      if i >= len / 2 then true
+      else
+        let left = (n / pow10 (len - 1 - i)) mod 10 in
+        let right = (n / pow10 i) mod 10 in
+        if left <> right then false
+        else check (i + 1)
+    in
+    check 0
+end
---
+++ lab1/task4/lib/solution3.ml
@@ -0,0 +1,23 @@
+open Common
+
+(* with map generate *)
+let rec ( -- ) (i1, j1) (i2, j2) = 
+  if i1 > i2 || j1 > j2 then []
+  else if j1 == j2 then (i1, j1) :: (i1 + 1, 100) -- (i2, j2)
+  else (i1, j1) :: (i1, j1 + 1) -- (i2, j2)
+let get_multiple (x, y) = x * y
+let max_element compare lst = 
+  match lst with 
+  | [] -> 0
+  | h :: t -> List.fold_left compare h t
+
+let result =
+  (100, 100) -- (999, 999)
+  |> List.map get_multiple
+  |> List.filter Common.is_palindrome
+  |> max_element max
+let _ = print_endline (string_of_int result)
+
+
+
+
